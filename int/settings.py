1323743fa2c3f22a0e27d14d4003168900d532cc
Eurico Dante <riicodt@gmail.com>
2025-09-19 16:57:08 -0300

    teste

--- int/settings.py
+++ int/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
