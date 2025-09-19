99b42285f60e61c13f86e20e8113e7081627d953
Eurico Dante <riicodt@gmail.com>
2025-09-19 16:08:19 -0300

    Update ALLOWED_HOSTS in settings.py to include production domains

--- int/settings.py
+++ int/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-7=sd^vwv+in5&os2t1zv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='www.gserp.com.br,gserp.com.br').split(',')
 
 # Login/Logout URLs
 LOGIN_URL = '/login/'
